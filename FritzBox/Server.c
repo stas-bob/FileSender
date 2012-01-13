3df3aeda375ca2065fd698c7e1840fc4836ba2ed
bline <oot.pac@googlemail.com>
2012-01-13 01:45:19 +0100

    server ausfallsicherer

--- FritzBox/Server.c
+++ FritzBox/Server.c
@@ -26,11 +26,13 @@
 #define RM_FILES 2
 #define BEGIN_DATA 3
 #define BEGIN_RM 4
+#define GET_DISK_SPACE 5
 #define STRING 2
 #define ERROR 3
 #define INTEGER 0
-#define GET_DISK_SPACE 5
 #define SKIP 14
+#define CONNECTION_ERROR 11
+#define INVALID_TYPE 12
 
 const int OK = 1;
 
@@ -43,27 +45,57 @@ typedef struct {
 } MyFile;
 
 
+/*
+ int RecvTimeout ( int s, char *buf, int len, int timeout ) {
+         fd_set fds ;
+         int n ;
+         struct timeval tv ;
+
+         // Set up the file descriptor set.
+         FD_ZERO(&fds) ;
+         FD_SET(s, &fds) ;
+
+         // Set up the struct timeval for the timeout.
+         tv.tv_sec = timeout ;
+         tv.tv_usec = 0 ;
+
+         // Wait until timeout or data received.
+         n = select ( s+1, &fds, NULL, NULL, &tv ) ;
+         if ( n == 0  ) return -2 ;                      // Timeout
+         if ( n == -1 ) return -1 ;                      // Error
+
+         // data must be here, so do a normal recv
+         return recv ( s, buf, len, 0 ) ;
+ } */
 
 void *receive(int cd, int *pType, void *vPtr) {
 	free(vPtr);	
-	recv(cd, pType, sizeof(int), 0);
+	int receivedBytes = recv(cd, pType, sizeof(int), 0);
+	if (receivedBytes == 0 || receivedBytes == -1) {
+		*pType = CONNECTION_ERROR;		
+		return 0;	
+	}
+	if (*pType != STRING && *pType != ERROR && *pType != INTEGER && *pType != SKIP) {
+		*pType = INVALID_TYPE;
+		return 0;	
+	}
 	if ((*pType & STRING) == STRING) {
 		int length = 0;
 		recv(cd, &length, sizeof(int), 0);
+		printf("length %d\n", length);
 		if (length > 200 || length == 0) {
 			printf("String is too long or empty\n");
 			vPtr = 0;
 		} else {
 			vPtr = malloc(length + 1);
 			if (vPtr != 0) {
-				memset(vPtr, 0, length + 1);
 				recv(cd, vPtr, length, 0);
+				*((char *)vPtr + length) = '\0';
 			}
 		}
 	}  else {
 		vPtr = malloc(sizeof(int));
 		if (vPtr != 0) {
-			memset(vPtr, 0, sizeof(int));
 			recv(cd, vPtr, sizeof(int), 0);
 		}
 	}
@@ -96,128 +128,129 @@ void errorHandleSend(int clientdescriptor, char *msg) {
 	printf("Error: %s\n", msg);
 }
 
+int testType(int cd, int type, void *vPtr) {
+	switch (type) {
+	case ERROR:
+		printf("client error received: %s\n", (char *)vPtr);
+		return 0;
+	case CONNECTION_ERROR:
+		printf("client connection broken\n");
+		return 0;
+	case INVALID_TYPE:
+		printf("Invalid type %d\n", type);
+		errorHandleSend(cd, "Invalid type");
+		return 0;
+	}
+	return 1;
+}
 
 void *saveFiles(int clientdescriptor, char *usbPath, void *vPtr) {	
 	char *errorStr = 0;
 	int type;
 	vPtr = receive(clientdescriptor, &type, vPtr);
+	if (!testType(clientdescriptor, type, vPtr)) return vPtr;
 	if (vPtr == 0) {
 		errorHandleSend(clientdescriptor, "pathscount ptr is null\0");	
 	} else {
-		if (type == ERROR) {
-			printf("client error received: %s\n", (char *)vPtr);
-		} else {
-			char *msg = "pathscount is ok\0";
-			sendNotSynced(clientdescriptor, msg, strlen(msg), STRING);
-			int pathsCount = *((int *)vPtr);
-			int h = 0;	
-			for (; h < pathsCount; h++) {
-				vPtr = receive(clientdescriptor, &type, vPtr);
-				if (vPtr == 0) {
-					errorHandleSend(clientdescriptor, "filescount ptr is null\0");
-					return vPtr;
-				} else {
-					if (type == ERROR) {
-						printf("client error received: %s\n", (char *)vPtr);
-						return vPtr;
+		char *msg = "pathscount is ok\0";
+		sendNotSynced(clientdescriptor, msg, strlen(msg), STRING);
+		int pathsCount = *((int *)vPtr);
+		int h = 0;	
+		for (; h < pathsCount; h++) {
+			vPtr = receive(clientdescriptor, &type, vPtr);
+			if (!testType(clientdescriptor, type, vPtr)) return vPtr;
+			if (vPtr == 0) {
+				errorHandleSend(clientdescriptor, "filescount ptr is null\0");
+				return vPtr;
+			} else {
+				char *msg = "filescount is ok\0";
+				sendNotSynced(clientdescriptor, msg, strlen(msg), STRING);
+				int filesCount = *((int *)vPtr);	
+				MyFile files[filesCount];	
+				int i = 0;		
+				for (;i < filesCount; i++) {
+					MyFile *file = &files[i];
+					initMyFile(file);
+					strcpy(file->fullName, usbPath);
+					vPtr = receive(clientdescriptor, &type, vPtr);
+					if (!testType(clientdescriptor, type, vPtr)) return vPtr;
+					if (vPtr == 0) {
+						errorHandleSend(clientdescriptor, "filename ptr is null\0");
+						return vPtr;	
 					} else {
-						char *msg = "filescount is ok\0";
+						char *msg = "filename ptr is ok\0";
 						sendNotSynced(clientdescriptor, msg, strlen(msg), STRING);
-						int filesCount = *((int *)vPtr);	
-						MyFile files[filesCount];	
-						int i = 0;		
-						for (;i < filesCount; i++) {
-							MyFile *file = &files[i];
-							initMyFile(file);
-							strcpy(file->fullName, usbPath);
-							vPtr = receive(clientdescriptor, &type, vPtr);
-							if (vPtr == 0) {
-								errorHandleSend(clientdescriptor, "filename ptr is null\0");
-								return vPtr;	
+						strcpy(file->fileName, (char *)vPtr);
+						strcat(file->fullName, file->fileName);
+						printf("received file name: %s\n", file->fileName);
+						vPtr = receive(clientdescriptor, &type, vPtr);
+						if (!testType(clientdescriptor, type, vPtr)) return vPtr;
+						if (vPtr == 0) {
+							errorHandleSend(clientdescriptor, "filelength ptr is null\0");
+							return vPtr;	
+						} else {
+							if (type == SKIP) {
+								printf("skipping file %s\n", file->fullName);
 							} else {
-								if (type == ERROR) {
-									printf("client error received: %s\n", (char *)vPtr);
-									return vPtr;			
+								char *msg = "filelength ptr is ok\0";
+								sendNotSynced(clientdescriptor, msg, strlen(msg), STRING);
+								file->fileLength = *((int *)vPtr);
+								printf("received file length: %d\n", file->fileLength);  
+
+								FILE *pFile = fopen(file->fullName, "w+b");
+								if (pFile == 0) {
+									errorHandleSend(clientdescriptor, strerror(errno));
+									return vPtr;
 								} else {
-									char *msg = "filename ptr is ok\0";
-									sendNotSynced(clientdescriptor, msg, strlen(msg), STRING);
-									strcpy(file->fileName, (char *)vPtr);
-									strcat(file->fullName, file->fileName);
-									printf("received file name: %s\n", file->fileName);
 									vPtr = receive(clientdescriptor, &type, vPtr);
+									if (!testType(clientdescriptor, type, vPtr)) return vPtr;
 									if (vPtr == 0) {
-										errorHandleSend(clientdescriptor, "filelength ptr is null\0");
-										return vPtr;	
+										errorHandleSend(clientdescriptor, "begin_data ptr is null\0");
+										fclose(pFile);
+										return vPtr;
 									} else {
-										if (type == ERROR) {
-											printf("client error received: %s\n", (char *)vPtr);
-											return vPtr;
-										} else {
-											if (type == SKIP) {
-												printf("skipping file %s\n", file->fullName);
-											} else {
-												char *msg = "filelength ptr is ok\0";
-												sendNotSynced(clientdescriptor, msg, strlen(msg), STRING);
-												file->fileLength = *((int *)vPtr);
-												printf("received file length: %d\n", file->fileLength);  
+										char *msg = "begin_data ptr is ok\0";
+										sendNotSynced(clientdescriptor, msg, strlen(msg), STRING);
+										int begin_data = *((int *)vPtr);
+										if (begin_data == BEGIN_DATA) {
+											printf("writing into: %s\n", file->fullName);
+											int bytesRead = 0;
 
-												FILE *pFile = fopen(file->fullName, "w+b");
-												if (pFile == 0) {
-													errorHandleSend(clientdescriptor, strerror(errno));
-													return vPtr;
+											while (bytesRead < file->fileLength) {
+												int remainingBytesCount = file->fileLength - bytesRead;
+												int actualBufferSize = 0;					
+												if (remainingBytesCount >= bufferSize) {
+													actualBufferSize = bufferSize;
 												} else {
-													vPtr = receive(clientdescriptor, &type, vPtr);
-													if (vPtr == 0) {
-														errorHandleSend(clientdescriptor, "begin_data ptr is null\0");
-														fclose(pFile);
-														return vPtr;
-													} else {
-														if (type == ERROR) {
-															printf("client error received: %s\n", (char *)vPtr);
-															return vPtr;						
-														} else {
-															char *msg = "begin_data ptr is ok\0";
-															sendNotSynced(clientdescriptor, msg, strlen(msg), STRING);
-															int begin_data = *((int *)vPtr);
-															if (begin_data == BEGIN_DATA) {
-																printf("writing into: %s\n", file->fullName);
-																int bytesRead = 0;
-
-																while (bytesRead < file->fileLength) {
-																	int remainingBytesCount = file->fileLength - bytesRead;
-																	int actualBufferSize = 0;					
-																	if (remainingBytesCount >= bufferSize) {
-																		actualBufferSize = bufferSize;
-																	} else {
-																		memset(buffer, 0, bufferSize);
-																		actualBufferSize = remainingBytesCount;
-																	}	
-																	int bytesRecv = recv(clientdescriptor, buffer, actualBufferSize, 0);
-																	fwrite(buffer, 1, bytesRecv, pFile);				
-																	bytesRead += bytesRecv;			
-																}
-																fputs("\0", pFile);
-																printf("finished writing %s\n", file->fullName);
-																sendNotSynced(clientdescriptor, "done\0", 5, STRING); 
-															} else {
-																errorHandleSend(clientdescriptor, "wrong integer for begin_data\0");
-																fclose(pFile);
-																return vPtr;
-															}
-															fclose(pFile);
-														}
-													}
-												}
+													memset(buffer, 0, bufferSize);
+													actualBufferSize = remainingBytesCount;
+												}	
+												int bytesRecv = recv(clientdescriptor, buffer, actualBufferSize, 0);
+												if (bytesRecv == 0 || bytesRecv == -1) {
+													printf("client connection broken\n");
+													return vPtr;
+												}																	
+												fwrite(buffer, 1, bytesRecv, pFile);				
+												bytesRead += bytesRecv;			
 											}
+											fputs("\0", pFile);
+											printf("finished writing %s\n", file->fullName);
+											sendNotSynced(clientdescriptor, "done\0", 5, STRING); 
+										} else {
+											errorHandleSend(clientdescriptor, "wrong integer for begin_data\0");
+											fclose(pFile);
+											return vPtr;
 										}
 									}
-								}	
-							}		
-						} //end for
-					}	
-				}
-			} // end for
-		}
+									fclose(pFile);
+								}
+							}
+						}	
+					}		
+				} //end for
+			}	
+		} // end for
+		
 	}
 	return vPtr;	
 }
@@ -232,39 +265,35 @@ void *rmFiles(int clientdescriptor, char *usbPath, void *vPtr) {
 		int begin_rm = 0;
 		int type;
 		vPtr = receive(clientdescriptor, &type, vPtr);
-				
+		if (!testType(clientdescriptor, type, vPtr)) return vPtr;		
 		if (vPtr == 0) {
 			errorHandleSend(clientdescriptor, "begin_rm ptr is null\0");	
 		} else {
-			if (type == ERROR) {
-				printf("client error received: %s\n", (char *)vPtr);	
-			} else {
-				char *msg = "begin_rm received\0";
-				sendNotSynced(clientdescriptor, msg, strlen(msg), STRING);
-				int begin_rm = *((int *)vPtr);
-				if (begin_rm == BEGIN_RM) {
-					struct dirent *entry = NULL;
-					struct stat statbuf;
-					   
-					while((entry = readdir(mydir))) {
-						char tmp[strlen(entry->d_name) + strlen(usbPath)];
+			char *msg = "begin_rm received\0";
+			sendNotSynced(clientdescriptor, msg, strlen(msg), STRING);
+			int begin_rm = *((int *)vPtr);
+			if (begin_rm == BEGIN_RM) {
+				struct dirent *entry = NULL;
+				struct stat statbuf;
+				   
+				while((entry = readdir(mydir))) {
+					char tmp[strlen(entry->d_name) + strlen(usbPath)];
 
-						strcpy(tmp, usbPath);
-						strcat(tmp, entry->d_name);
-						stat(tmp, &statbuf);
-		   				if (S_ISREG(statbuf.st_mode)) {
-							if (remove(tmp) == -1) {
-					  			errorHandleSend(clientdescriptor, strerror(errno));
-								closedir(mydir);					
-								return;
-							}
+					strcpy(tmp, usbPath);
+					strcat(tmp, entry->d_name);
+					stat(tmp, &statbuf);
+	   				if (S_ISREG(statbuf.st_mode)) {
+						if (remove(tmp) == -1) {
+				  			errorHandleSend(clientdescriptor, strerror(errno));
+							closedir(mydir);					
+							return;
 						}
 					}
-					printf("deleted\n");
-					sendNotSynced(clientdescriptor, "deleted\0", 8, STRING);
-				} else {
-					errorHandleSend(clientdescriptor, "wrong integer for begin_rm\0");
 				}
+				printf("deleted\n");
+				sendNotSynced(clientdescriptor, "deleted\0", 8, STRING);
+			} else {
+				errorHandleSend(clientdescriptor, "wrong integer for begin_rm\0");
 			}
 		}
 		closedir(mydir);	
@@ -291,7 +320,6 @@ void getDiskSpace(int cd, char *usbPath) {
    else {
       errorHandleSend(cd, "Failed to stat\0");
    }
-
 }
 
 
@@ -355,14 +383,13 @@ int main(int argc, char **argv) {
 		printf("incoming connection from %s at %s", inet_ntoa(clientSocket.sin_addr), ctime(&rawtime));
 		int type;		
 		vPtr = receive(clientdescriptor, &type, vPtr);
-		if (vPtr == 0) {
-			errorHandleSend(clientdescriptor, "password ptr is null\0");	
-		} else {
-			if (type == ERROR) {
-				printf("client error received: %s\n", (char *)vPtr);		
+		if (testType(clientdescriptor, type, vPtr)) {
+			if (vPtr == 0) {
+				errorHandleSend(clientdescriptor, "password ptr is null\0");	
 			} else {
 				char pw[16];
 				char *password = (char *)vPtr;
+				printf("client tries password: %s\n", password);
 				FILE *pFile = fopen("password", "r");
 				if (pFile == 0) {
 					errorHandleSend(clientdescriptor, "password file not found\0");
@@ -389,15 +416,16 @@ int main(int argc, char **argv) {
 									default: break;
 								}
 							}
-										
+									
 						}
 					} else {
 						errorHandleSend(clientdescriptor, "wrong password\0");
 					}
 					fclose(pFile);
-				}
-			}		
+				}		
+			}
 		}
+		printf("close client\n");										
 		free(vPtr);
 	    	close(clientdescriptor);
 	    	exit(EXIT_SUCCESS);
---
+++ src/de/stas/DataException.java
@@ -0,0 +1,14 @@
+package de.stas;
+
+public class DataException extends ServerException {
+
+	public DataException(String str) {
+		super(str);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
